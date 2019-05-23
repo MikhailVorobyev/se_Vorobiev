6334b091de2eb37be2c83d3c8b0b6d661ce3707e
mikhail.vorobiev <mvorobyev1782@gmail.com>
2019-05-23 13:28:13 +0300

    module6

---
+++ src/sef/module6/activity/Point2DImpl.java
@@ -0,0 +1,134 @@
+package sef.module6.activity;
+
+
+public class Point2DImpl implements Point2D {
+	
+	private double x;
+	private double y;
+	
+	/**
+	 * Creates a Point2D object at a default location (0,0)
+	 */
+	public Point2DImpl(){
+		this(0.0, 0.0);
+	}
+	
+	/**
+	 * Create a Point2D object that represents a 2D coordinate specified 
+	 * by the constructor parameters
+	 * 
+	 * @param x coordinate of the point along the x-axis
+	 * @param y coordinate of the point along the y-axis
+	 */
+	public Point2DImpl(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#move(double, double)
+	 */
+	public final void move(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#setX(double)
+	 */
+	public void setX(double x){
+		this.x = x;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#setY(double)
+	 */
+	public void setY(double y){
+		this.y = y;
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#getX()
+	 */
+	public double getX(){
+	
+		return x;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#getY()
+	 */
+	public double getY(){
+		
+		return y;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#translate(double, double)
+	 */
+	public final void translate(double dx, double dy){
+		x += dx;
+		y += dy;
+	}
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equals(Object p){
+		if (this == p) {
+			return true;
+		}
+		if (this.getClass() != p.getClass()) {
+			return false;
+		}
+
+		Point2DImpl point2D = (Point2DImpl) p;
+		return equals(point2D.x, point2D.y);
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#equals(double, double)
+	 */
+	public boolean equals(double x2, double y2){
+		if (x == x2 && y == y2) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#getDistance(sef.module5.activity.Point2D)
+	 */
+	public final double getDistance(Point2D p){
+		
+		return getDistance(p.getX(), p.getY());
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point2D#getDistance(double, double)
+	 */
+	public final double getDistance(double x2, double y2){
+		double dx = x2 - x;
+		double dy = y2 - y;
+
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+	
+	
+	
+}
---
+++ src/sef/module6/activity/Point3DImpl.java
@@ -0,0 +1,118 @@
+package sef.module6.activity;
+
+/**
+ * @author  John Doe
+ * 
+ * This class represents a point in 3D space.  The coordinates are represented by
+ * a set of values x, y an z that represents the coordinates along each plane
+ */
+public class Point3DImpl extends Point2DImpl implements Point3D{
+	
+	@SuppressWarnings("unused")
+	private double z;
+	
+	/**
+	 * Creates a Point3D object with the default coordinate of 0,0,0 
+	 *
+	 */
+	public Point3DImpl(){
+		this(0.0, 0.0, 0.0);
+	}
+	
+	/**
+	 * Creates a Point3D object at the specified coordinates
+	 * 
+	 * @param x coordinate along the x axis
+	 * @param y coordinate along the y axis
+	 * @param z coordinate along the z axis
+	 */
+	public Point3DImpl(double x, double y, double z){
+		super(x, y);
+		this.z = z;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#setZ(double)
+	 */
+	public final void setZ(double z){
+		this.z = z;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#getZ()
+	 */
+	public final double getZ(){
+		return z;
+	}
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#move(double, double, double)
+	 */
+	public void move(double x2, double y2, double z2){
+		super.move(x2, y2);
+		z = z2;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#translate(double, double, double)
+	 */
+	public void translate(double x2, double y2, double z2){
+		super.translate(x2, y2);
+		z += z2;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#equals(double, double, double)
+	 */
+	public boolean equals(double x2, double y2, double z2){
+		return super.equals(x2, y2) && z == z2;
+
+	}
+	
+	
+	/**
+	 * Compares if the coordinates are equal to the coordinates specified by the parameter
+	 * 
+	 * @param p the coordinates to compare
+	 * 
+	 * @return true if the parameter is an instance of Point3D and contain the same coordinates, false otherwise
+	 */
+	public boolean equals(Object p){
+		if (this == p) {
+			return true;
+		}
+
+		if (this.getClass() != p.getClass()) {
+			return false;
+		}
+
+		Point3DImpl point3D = (Point3DImpl) p;
+		return super.equals(point3D.getX(), point3D.getY()) && z == point3D.z;
+		
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#getDistance(sef.module5.activity.Point3D)
+	 */
+	public double getDistance(Point3D p){
+		return getDistance(p.getX(), p.getY(), p.getZ());
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see sef.module5.activity.Point3D#getDistance(double, double, double)
+	 */
+	public double getDistance(double x2, double y2, double z2){
+		double dx = x2 - getX();
+		double dy = y2 - getY();
+		double dz = z2 - z;
+
+		return Math.sqrt(dx * dx + dy * dy + dz * dz);
+	
+	}
+}
